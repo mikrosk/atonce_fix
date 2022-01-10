d62899f2b10cc872c5515cf5bc7f879817009741
Miro Kropacek <miro.kropacek@gmail.com>
2022-01-10 16:22:19 +0100

    Bootloader + simplification
    ... but still refuses to boot?!

--- create_msdos_img.sh
+++ create_msdos_img.sh
@@ -17,24 +17,23 @@ disk_image=${3}
 tmp_file=$(mktemp)
 dd if="$disk_image" of="$tmp_file" bs=512 skip=$(($start_sector+1)) count="$count" 2> /dev/null
 
+echo "Starting cfdisk..."
 cfdisk "$tmp_file"
 
-echo
-fdisk -l "$tmp_file" | grep FAT16 | while IFS=' ' read -r device start end sectors size id fstype
+fdisk -c=dos -b 512 -t dos -o Start,Sectors,Type -l "$tmp_file" | grep FAT16 | while IFS=' ' read -r start sectors fstype
 do
-	# avoid losetup / mount with sudo
-	echo "Processing volume at sector ${start}..."
+    echo "Processing MS-DOS volume at sector ${start} of size ${sectors} sectors..."
 	echo "Mount as: sudo mount -o loop,offset=$((($start_sector+1+$start)*512)) \"$disk_image\" /mnt"
-	volume=$(mktemp)
-	dd if="$tmp_file" of="$volume" bs=512 skip="$start" count="$sectors" 2> /dev/null
-	mkdosfs "$volume" > /dev/null
-	dd if="$volume" of="$tmp_file" bs=512 seek="$start" count="$sectors" conv=notrunc 2> /dev/null
-	echo
-	rm "$volume"
+
+	# DOS FORMAT expects DOS FDISK to clear the first 512 bytes (man fdisk) but let's clear the whole volume to have a fresh start
+	dd if=/dev/zero of="$tmp_file" bs=512 seek="$start" count="$sectors" conv=notrunc 2> /dev/null
+	# block count is specified in KiB (1024)
+	mkdosfs --offset="$start" "$tmp_file" $(($sectors*512/1024)) > /dev/null
+	# WARNING: hardcoded offset (assumes 0xeb 0x3c 0x90 at $start)
+	dd if=bootloader.bin of="$tmp_file" bs=1 seek=$(($start*512+62)) conv=notrunc 2> /dev/null
 done 
 
-#exit
-
+echo
 read -p "Write changes back to $disk_image? (y/n) " yn
 case $yn in
 	[Yy]* )
