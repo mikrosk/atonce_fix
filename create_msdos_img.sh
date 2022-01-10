9b2bfa6ca5b2ffd2573e1b9716703532447b735f
Miro Kropacek <miro.kropacek@gmail.com>
2022-01-10 17:09:31 +0100

    -g heads/sectors_per_track did the trick
    It's recognised, it's installable, it boots!

--- create_msdos_img.sh
+++ create_msdos_img.sh
@@ -23,12 +23,14 @@ cfdisk "$tmp_file"
 fdisk -c=dos -b 512 -t dos -o Start,Sectors,Type -l "$tmp_file" | grep FAT16 | while IFS=' ' read -r start sectors fstype
 do
     echo "Processing MS-DOS volume at sector ${start} of size ${sectors} sectors..."
+    echo
 	echo "Mount as: sudo mount -o loop,offset=$((($start_sector+1+$start)*512)) \"$disk_image\" /mnt"
 
 	# DOS FORMAT expects DOS FDISK to clear the first 512 bytes (man fdisk) but let's clear the whole volume to have a fresh start
 	dd if=/dev/zero of="$tmp_file" bs=512 seek="$start" count="$sectors" conv=notrunc 2> /dev/null
 	# block count is specified in KiB (1024)
-	mkdosfs --offset="$start" "$tmp_file" $(($sectors*512/1024)) > /dev/null
+	# TODO: is -h always $start and not $start+previous_partition_size? is sectors per track == $start?
+	mkdosfs -a -g 64/34 -h "$start" --offset="$start" "$tmp_file" $(($sectors*512/1024)) > /dev/null
 	# WARNING: hardcoded offset (assumes 0xeb 0x3c 0x90 at $start)
 	dd if=bootloader.bin of="$tmp_file" bs=1 seek=$(($start*512+62)) conv=notrunc 2> /dev/null
 done 
